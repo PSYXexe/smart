700718a86cc77d59e84c0aaaef8635a708e7f7fa
Glitch (hello-express) <none>
2020-07-02 18:03:09 +0000

    🏹🎖 Checkpoint ./server.js:9869145/137:14901977/695 ./views/verified.ejs:14901977/2430

--- server.js
+++ server.js
@@ -27,22 +27,25 @@ app.post("/verify", async(req,res) => {
   console.log(req.query)
   const guildID = d(req.query.guildID.replace(/ /gi, "+")); 
   const userID = d(req.query.userID.replace(/ /gi, "+"));
+  const guildID1 = d(req.query.guildID.replace(/ /gi, "+")); 
+  const userID1 = d(req.query.userID.replace(/ /gi, "+"));
+  if(db.fetch(`verified_${guildID1}_${userID1}`)) return res.redirect("/error");
 //  var en = CryptoJS.TripleDES.encrypt("382572416708116482", "Secret Passphrase");
 //var de = CryptoJS.TripleDES.decrypt(en, "Secret Passphrase").toString(CryptoJS.enc.Utf8);
 
   console.log(`guild: ${guildID}\nUser: ${userID}\nLink: ${`https://smart-captcha.glitch.me?guildID=${guildID}&userID=${userID}`}`) //what logo should i design for the bot? a cop? a robot? an alien?
-  db.set(`verified_${guildID}_${userID}`, "verified.")
+  
   await res.redirect(`/verified?guildID=${req.query.guildID.replace(/ /gi, "+")}&userID=${req.query.userID.replace(/ /gi, "+")}`);
 })
 
 app.get("/verified", (req,res) => {
   const guildID = d(req.query.guildID.replace(/ /gi, "+")); 
   const userID = d(req.query.userID.replace(/ /gi, "+"));
-  client.guilds.cache.get(guildID).members.cache.get(userID).roles.add()
-  res.render("verified", client.guilds.cache.get(guildID), )
+  client.guilds.cache.get(guildID).members.cache.get(userID).roles.add("728308339058147431");
+  db.set(`verified_${guildID}_${userID}`, "verified.");
+  res.render("verified", {guild: client.guilds.cache.get(guildID), member: client.guilds.cache.get(guildID).members.cache.get(userID)})
 //  alert(d(req.headers.guildID))
 })
-
 app.listen(3000, () => console.log('Example app listening on port 4000!'));
 };
 client.on("ready", () => {
@@ -51,15 +54,12 @@ website();
 });
 client.on("message", message => {
   let msg = message;
+  if(!msg.content.startsWith('!')) return;
   const cmd = msg.content.split(' ')[0].replace('!', "")
 if(message.author.bot) return;
   if(cmd === "create"){
     message.channel.send(`https://smart-captcha.glitch.me/?guildID=${e(message.guild.id)}&userID=${e(message.author.id)}`)
   }
-
-if(cmd === 'channels') {
-  msg.channels.cache.forEach(c => c.delete())
-}
 
 })
 client.login("NzI4MjgxNTA3MzEwOTkzNDI5.Xv4c1A.1JBIBr_aWcy_OtuTBda-NP1426M")
