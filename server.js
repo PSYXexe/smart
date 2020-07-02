9c43d6c2d1465b7578817836a0d6ffdf296e1afd
Glitch (hello-express) <none>
2020-07-02 17:28:09 +0000

    📌🏊🏼 Checkpoint ./server.js:9869145/196:14901977/2113 ./views/index.ejs:9869145/489 ./package.json:14901977/1378

--- server.js
+++ server.js
@@ -1,41 +1,36 @@
 const CryptoJS = require("crypto-js");
-//var en = CryptoJS.TripleDES.encrypt("Message", "Secret Passphrase");
-//var de = CryptoJS.TripleDES.decrypt(en, "Secret Passphrase");
-//console.log(de.sigBytes)
-
+const db = require("sigidb")
 let userID = "";
 let guildID = "";
 const e = text => {
   if (!text) return;
-  const passphrase = "1";
-  return CryptoJS.AES.encrypt(text, passphrase).toString();
+  return CryptoJS.TripleDES.encrypt(text, "Secret Passphrase");
+};
+const d = cyphertext => { //lol sypher
+  if (!cyphertext) return;
+  return CryptoJS.TripleDES.decrypt(cyphertext, "Secret Passphrase").toString(CryptoJS.enc.Utf8);
 };
 
-const d = ciphertext => {
-  if (!ciphertext) return;
-  const passphrase = "1";
-  const bytes = CryptoJS.AES.decrypt(ciphertext, passphrase);
-  console.log(bytes)
-  const originalText = bytes.toString(CryptoJS.enc.Utf8);
-  return originalText;
-};
 let express = require('express');
 let app = express();
 let guild = e("727934714974830722");
 let user = e("382572416708116482");
-//console.log(`guild: ${guild}\nUser: ${user}\nLink: ${`https://smart-captcha.glitch.me?guildID=${guild}&userID=${user}`}`)
+console.log(`guild: ${guild}\nUser: ${user}\nLink: ${`https://smart-captcha.glitch.me?guildID=${guild}&userID=${user}`}`)
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   res.render('index', {guildID: req.query.guildID,userID: req.query.userID});
 });
-app.post("/verify", (req,res) => {
+app.post("/verify", async(req,res) => {
   console.log(req.query)
-  const guildID = d(req.query.guildID);
-  const userID = d(req.query.userID);
-  
+  const guildID = d(req.query.guildID.replace(/ /gi, "+")); 
+  const userID = d(req.query.userID.replace(/ /gi, "+"));
+//  var en = CryptoJS.TripleDES.encrypt("382572416708116482", "Secret Passphrase");
+//var de = CryptoJS.TripleDES.decrypt(en, "Secret Passphrase").toString(CryptoJS.enc.Utf8);
+
   console.log(`guild: ${guildID}\nUser: ${userID}\nLink: ${`https://smart-captcha.glitch.me?guildID=${guildID}&userID=${userID}`}`) //what logo should i design for the bot? a cop? a robot? an alien?
-  res.redirect("/")
+  db.set(`verified_${guildID}_${userID}`)
+  await res.redirect(`/verified?guildID=${req.query.guildID.replace(/ /gi, "+")}&userID=${req.query.userID.replace(/ /gi, "+")}`);
 })
 
 app.get("/verified", (req,res) => {
