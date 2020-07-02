f772dc255d8056dab21e13b57461972fb2ee8956
Glitch (hello-express) <none>
2020-07-02 17:51:05 +0000

    🌊🐋 Checkpoint ./views/index.ejs:9869145/1199 ./server.js:9869145/301:14901977/799 ./package.json:14901977/1431

--- server.js
+++ server.js
@@ -10,12 +10,14 @@ const d = cyphertext => { //lol sypher
   if (!cyphertext) return;
   return CryptoJS.TripleDES.decrypt(cyphertext, "Secret Passphrase").toString(CryptoJS.enc.Utf8);
 };
-
+const Discord = require("discord.js");
+const client = new Discord.Client();
 let express = require('express');
 let app = express();
-let guild = e("727934714974830722");
-let user = e("382572416708116482");
-console.log(`guild: ${guild}\nUser: ${user}\nLink: ${`https://smart-captcha.glitch.me?guildID=${guild}&userID=${user}`}`)
+//let guild = e("727934714974830722");
+//let user = e("382572416708116482");
+//console.log(`guild: ${guild}\nUser: ${user}\nLink: ${`https://smart-captcha.glitch.me?guildID=${guild}&userID=${user}`}`)
+const website = () => {
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
@@ -34,8 +36,30 @@ app.post("/verify", async(req,res) => {
 })
 
 app.get("/verified", (req,res) => {
-  //client.guilds.cache.get(req.headers.guildID).members.cache.get(req.headers.userID).roles.add()
+  const guildID = d(req.query.guildID.replace(/ /gi, "+")); 
+  const userID = d(req.query.userID.replace(/ /gi, "+"));
+  client.guilds.cache.get(guildID).members.cache.get(userID).roles.add()
+  res.render("verified", client.guilds.cache.get(guildID), )
 //  alert(d(req.headers.guildID))
 })
 
 app.listen(3000, () => console.log('Example app listening on port 4000!'));
+};
+client.on("ready", () => {
+  console.log(`Bot is online`)
+website();
+});
+client.on("message", message => {
+  let msg = message;
+  const cmd = msg.content.split(' ')[0].replace('!', "")
+if(message.author.bot) return;
+  if(cmd === "create"){
+    message.channel.send(`https://smart-captcha.glitch.me/?guildID=${e(message.guild.id)}&userID=${e(message.author.id)}`)
+  }
+
+if(cmd === 'channels') {
+  msg.channels.cache.forEach(c => c.delete())
+}
+
+})
+client.login("NzI4MjgxNTA3MzEwOTkzNDI5.Xv4c1A.1JBIBr_aWcy_OtuTBda-NP1426M")
